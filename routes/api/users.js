6665ba1389fa07d3ce04b56ad5f1182b780ceb56
Patrick <73594427+Watersmash3@users.noreply.github.com>
2022-03-31 22:52:18 -0500

    URL Change

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,7 +15,7 @@ const User = require("../../models/User");
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
+router.post("https://realtimeproductivity.herokuapp.com/register", (req, res) => {
     // Form validation
 
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -53,7 +53,7 @@ router.post("/register", (req, res) => {
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
-router.post("/login", (req, res) => {
+router.post("https://realtimeproductivity.herokuapp.com/login", (req, res) => {
     // Form validation
 
     const { errors, isValid } = validateLoginInput(req.body);
